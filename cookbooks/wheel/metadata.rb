be8b25878eb1a3dff9c8ebc10c26eb0de390f6b1
Max Korenkov <mkorenkov@gmail.com>
2012-02-16 23:24:23 +0200

    initial commit

---
+++ cookbooks/wheel/metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "Max Korenkov"
+maintainer_email "mkorenkov@gmail.com"
+license          "Apache2.0"
+description      "Installs OpenStack using stack.sh and runs tempest integration test suite on it."
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.1"
---
+++ cookbooks/wheel/recipes/stack.rb
@@ -0,0 +1,74 @@
+# -*- mode: ruby -*-
+# # vi: set ft=ruby :
+#
+# Cookbook Name:: wheel
+# Recipe:: stack 
+#
+
+include_recipe "git::default"
+include_recipe "mysql::server"
+include_recipe "rabbitmq::default"
+
+user node[:wheel][:username] do
+    comment "Wheel User"
+    home "/home/#{node[:wheel][:username]}"
+    shell "/bin/bash"
+end
+
+directory "/home/#{node[:wheel][:username]}" do
+    owner node[:wheel][:username]
+    group node[:wheel][:username]
+    mode "0755"
+    action :create
+end
+
+directory "/home/#{node[:wheel][:username]}/.ssh" do
+    owner node[:wheel][:username]
+    group node[:wheel][:username]
+    mode "0755"
+    action :create
+end
+
+cookbook_file "/home/#{node[:wheel][:username]}/.ssh/config" do
+    source "config"
+    owner node[:gitolite][:wheel]
+    group node[:gitolite][:wheel]
+    action :create_if_missing
+end
+
+cookbook_file "/home/#{node[:wheel][:username]}/.ssh/id_rsa" do
+    source "id_rsa"
+    owner node[:wheel][:username]
+    group node[:wheel][:username]
+    mode "0600"
+    action :create_if_missing
+end
+
+cookbook_file "/home/#{node[:wheel][:username]}/.ssh/id_rsa.pub" do
+    source "id_rsa.pub"
+    owner node[:wheel][:username]
+    group node[:wheel][:username]
+    action :create_if_missing
+end
+
+git "/home/#{node[:wheel][:username]}/devstack" do
+    repository node[:stack][:repository_url]
+    reference "master"
+    action :sync
+    user node[:wheel][:username]
+end
+
+cookbook_file "/home/#{node[:wheel][:username]}/devstack/localrc" do
+    source "localrc"
+    owner node[:wheel][:username]
+    group node[:wheel][:username]
+    action :create_if_missing
+end
+
+execute "./stack.sh" do
+    user node[:wheel][:username]
+    environment ({"HOME" => "/home/#{node[:wheel][:username]}"})
+    cwd "/home/#{node[:wheel][:username]}/devstack"
+    not_if {File.exists?("/opt/stack/devstack")}
+end
+
